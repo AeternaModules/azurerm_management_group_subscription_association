8091fd7b0c9cd1c319cda59b0718c1fcc316e25d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "management_group_subscription_associations_id" {
+  description = "Map of id values across all management_group_subscription_associations, keyed the same as var.management_group_subscription_associations"
+  value       = { for k, v in azurerm_management_group_subscription_association.management_group_subscription_associations : k => v.id }
+}
 output "management_group_subscription_associations_management_group_id" {
   description = "Map of management_group_id values across all management_group_subscription_associations, keyed the same as var.management_group_subscription_associations"
   value       = { for k, v in azurerm_management_group_subscription_association.management_group_subscription_associations : k => v.management_group_id }
