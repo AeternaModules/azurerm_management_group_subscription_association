425c5016a83363a7d6e977999db16fe80420bcd4
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "management_group_subscription_associations_id" {
   description = "Map of id values across all management_group_subscription_associations, keyed the same as var.management_group_subscription_associations"
-  value       = { for k, v in azurerm_management_group_subscription_association.management_group_subscription_associations : k => v.id }
+  value       = { for k, v in azurerm_management_group_subscription_association.management_group_subscription_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "management_group_subscription_associations_management_group_id" {
   description = "Map of management_group_id values across all management_group_subscription_associations, keyed the same as var.management_group_subscription_associations"
-  value       = { for k, v in azurerm_management_group_subscription_association.management_group_subscription_associations : k => v.management_group_id }
+  value       = { for k, v in azurerm_management_group_subscription_association.management_group_subscription_associations : k => v.management_group_id if v.management_group_id != null && length(v.management_group_id) > 0 }
 }
 output "management_group_subscription_associations_subscription_id" {
   description = "Map of subscription_id values across all management_group_subscription_associations, keyed the same as var.management_group_subscription_associations"
-  value       = { for k, v in azurerm_management_group_subscription_association.management_group_subscription_associations : k => v.subscription_id }
+  value       = { for k, v in azurerm_management_group_subscription_association.management_group_subscription_associations : k => v.subscription_id if v.subscription_id != null && length(v.subscription_id) > 0 }
 }
 
